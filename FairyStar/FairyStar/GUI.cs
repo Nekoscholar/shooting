c2d6187e97269beb41e920fddb481d9a219a2f1d
Nekoscholar <srobserver62@gmail.com>
2016-12-31 22:40:19 +0900

    aa
    aaa

---
+++ FairyStar/FairyStar/GUI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FairyStar
+{
+    public partial class GUI : Form
+    {
+        public GUI()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.DoubleBuffer, true);
+            SetBounds(0, 0, 1280, 720);
+        }
+
+        
+    }
+}
---
+++ FairyStar/FairyStar/PlayArea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FairyStar
+{
+    class PlayArea
+    {
+    }
+
+    public partial class GUI : Form
+    {
+        private void PlayArea_paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
